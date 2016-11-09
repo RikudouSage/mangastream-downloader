4c30f2b47d76912cc1ec9afef405c8baa1e2a653
Dominik Chrastecky <dominik@devmates.com>
2016-11-09 12:46:42 +0100

    windows open file handler

--- sources/misctools.cpp
+++ sources/misctools.cpp
@@ -1,6 +1,14 @@
 #include "headers/misctools.h"
+#include <QDebug>
+
+#if defined(Q_OS_WIN)
+    #define PREFIX "file:///"
+#else
+    #define PREFIX "file://"
+#endif
 
 bool MiscTools::openDirectory(QString directory) {
-    directory = "file://"+directory;
+    qDebug() << PREFIX;
+    directory = PREFIX+directory;
     return QDesktopServices::openUrl(QUrl(directory));
 }
